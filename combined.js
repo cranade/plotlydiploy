266605f82b3f8b43a270990d42d42031f1037631
cranade <chanderranade@gmail.com>
2021-11-19 20:53:14 -0500

    November 19, 8:52 PM

--- combined.js
+++ combined.js
@@ -17,6 +17,8 @@ function init() {
         var firstSample = sampleNames[0];
         buildCharts(firstSample);
         buildMetadata(firstSample);
+        buildGauge(firstSample);
+        buildBubble(firstSample);
     });
 }
 
@@ -38,6 +40,8 @@ function optionChanged(newSample) {
     buildGauge(newSample);
 
 }
+
+
 // Demographics Panel 
 function buildMetadata(sample) {
     d3.json("samples.json").then((data) => {
